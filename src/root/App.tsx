19d6bb2440e4c7d64567ac0693957c92ee79d393
Ilemar <ilemarandrade@gmail.com>
2023-03-15 15:36:46 -0400

    feat: improve of App.tsx to show the app

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,23 +1,18 @@
 import "../App.scss";
+import Section from "../components/Section";
+import MainLayout from "../layout/MainLayout";
+import Providers from "./Providers";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Providers>
+      <MainLayout>
+        <Section>
+          <Routes />
+        </Section>
+      </MainLayout>
+    </Providers>
   );
 }
 
