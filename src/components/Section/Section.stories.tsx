0bf2c2b8a225be22eddd2fe27964b3113ba6c449
Ilemar <ilemarandrade@gmail.com>
2023-03-15 13:11:22 -0400

    feat: add stories Section

---
+++ src/components/Section/Section.stories.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Section from "./index";
+import Card from "../Card";
+
+export default {
+  title: "Example/Section",
+  component: Section,
+} as ComponentMeta<typeof Section>;
+
+const Template: ComponentStory<typeof Section> = (args) => (
+  <Section {...{ ...args }} />
+);
+
+export const SectionTemplate = Template.bind({});
+SectionTemplate.args = {
+  children: (
+    <>
+      <Card
+        {...{
+          img: "https://placekitten.com/200/200",
+          name: "Beth Smith",
+          origin: "Alive - Human",
+          open: true,
+          id: 1,
+          onClick: (id) => {
+            alert(id);
+          },
+        }}
+      />
+    </>
+  ),
+};
