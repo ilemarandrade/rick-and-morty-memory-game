9cc4c155913c1eff723304eac5f134636a47458c
Ilemar <ilemarandrade@gmail.com>
2023-03-15 17:37:35 -0400

    feat: create GameOver page

--- src/pages/GameOver/index.tsx
+++ src/pages/GameOver/index.tsx
@@ -1,3 +1,23 @@
-const GameOver = () => <>GameOver</>;
+import { NavLink } from "react-router-dom";
+import Button from "../../components/Button";
+import routes from "../../constants/routes";
+import classes from "./GameOver.module.scss";
+const GameOver = () => {
+  return (
+    <div className={classes.root}>
+      <h2 className="mb-1 font-primary">¡Felicitaciones!</h2>
+      <p className="mb-1-5">Terminaste el juego con 15 turnos</p>
+      <div className={`d-flex jc-space-between ${classes.containerButtons}`}>
+        <Button label="Repetir" component={NavLink} to={routes.PLAY} />
+        <Button
+          label="Inicio"
+          component={NavLink}
+          to={routes.HOME}
+          color="secondary"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default GameOver;
