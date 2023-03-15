216abf86df919c751bdeaab9604546c94098863a
Ilemar <ilemarandrade@gmail.com>
2023-03-15 17:18:15 -0400

    feat: build play page

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -1,3 +1,16 @@
-const Play = () => <>Play</>;
+import GameBoard from "../../components/GameBoard";
+import data from "../../__test__/data/characters.json";
+
+const Play = () => {
+  return (
+    <>
+      <div className="d-flex jc-space-between">
+        <h2 className="mb-1-5">Aciertos:</h2>
+        <h2 className="mb-1-5">Turnos:</h2>
+      </div>
+      <GameBoard data={data.results} />
+    </>
+  );
+};
 
 export default Play;
