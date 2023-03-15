f8c7bf8440b8c62bbf5bafd73095c8f85fb40da8
Ilemar <ilemarandrade@gmail.com>
2023-03-14 23:00:49 -0400

    feat: customRender to test

---
+++ src/__test__/utils/customRender.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+
+const AllTheProviders = ({ children }) => {
+  return <BrowserRouter>{children}</BrowserRouter>;
+};
+
+const customRender = (ui, { ...options } = {}) =>
+  render(ui, {
+    wrapper: ({ children }) => <AllTheProviders children={children} />,
+    ...options,
+  });
+
+// re-export everything
+export * from "@testing-library/react";
+
+// override render method
+export { customRender as render };
