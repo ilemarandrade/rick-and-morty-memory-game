688785f8d8d8cd021dacdc85d5a37dcc488ee322
Ilemar <ilemarandrade@gmail.com>
2023-03-15 13:09:19 -0400

    feat: test to Section component

---
+++ src/components/Section/Section.spec.tsx
@@ -0,0 +1,14 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "../../__test__/utils/customRender";
+import Section from "./index";
+
+describe("<Section />", () => {
+  it("Should show the Section", () => {
+    render(
+      <Section>
+        <div>I am a children</div>
+      </Section>
+    );
+    expect(screen.getByText("I am a children")).toBeInTheDocument();
+  });
+});
