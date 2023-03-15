7fd03a98159c4af9f55db984fb36bd9055040c72
Ilemar <ilemarandrade@gmail.com>
2023-03-15 10:04:09 -0400

    feat: add test of Button component when its props component is a button tag

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,16 +1,23 @@
 import "@testing-library/jest-dom";
 import Button from "./index";
 import { NavLink } from "react-router-dom";
-import { render, screen } from "../../__test__/utils/customRender";
+import { render, screen, fireEvent } from "../../__test__/utils/customRender";
 
 const propsAsAnchor = {
   label: "Label",
   href: "/iAmHref",
+  component: "a",
 };
 const propsAsNavLink = {
   label: "Label With NavLink",
   to: "/iAmTo",
   component: NavLink,
+};
+const propsAsButton = {
+  label: "Label to Button",
+  onClick: jest.fn(() => {
+    console.log("llamada");
+  }),
 };
 describe("<Button />", () => {
   describe("Should show variants buttons", () => {
@@ -25,7 +32,7 @@ describe("<Button />", () => {
       expect(button).toHaveAttribute("color", "secondary");
     });
   });
-  it("Should show the component as <a>", () => {
+  it("Should show the component as <a />", () => {
     render(<Button {...{ ...propsAsAnchor }} />);
     expect(screen.getByText(propsAsAnchor.label)).toBeInTheDocument();
     expect(screen.getByRole("link")).toHaveTextContent(propsAsAnchor.label);
@@ -34,10 +41,17 @@ describe("<Button />", () => {
       propsAsAnchor.href
     );
   });
-  it("Should show the component as <NavLink>", () => {
+  it("Should show the component as <NavLink />", () => {
     render(<Button {...{ ...propsAsNavLink }} />);
     expect(screen.getByText(propsAsNavLink.label)).toBeInTheDocument();
     expect(screen.getByRole("link")).toHaveTextContent(propsAsNavLink.label);
     expect(screen.getByRole("link")).toHaveAttribute("href", propsAsNavLink.to);
+  });
+  it("Should show the component as <button />", () => {
+    render(<Button {...{ ...propsAsButton }} />);
+    expect(screen.getByText(propsAsButton.label)).toBeInTheDocument();
+    expect(screen.getByRole("button")).toHaveTextContent(propsAsButton.label);
+    fireEvent.click(screen.getByRole("button"));
+    expect(propsAsButton.onClick).toHaveBeenCalledTimes(1);
   });
 });
