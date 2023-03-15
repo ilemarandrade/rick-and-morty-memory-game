f945aef4354bb0c5ee5ad1f9b5fa7e8b9a5c387f
Ilemar <ilemarandrade@gmail.com>
2023-03-15 12:29:17 -0400

    feat: add test of Header component

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "../../__test__/utils/customRender";
+import Header from "./index";
+
+describe("<Header />", () => {
+  it("Should show the Header", () => {
+    render(<Header />);
+    expect(
+      screen.getByRole("heading", { name: "Juego de memoria" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("img")).toHaveAttribute("src", "title.png");
+  });
+});
