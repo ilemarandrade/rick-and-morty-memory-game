cad3b10eceba627803ef851e74f45011a4e86040
Ilemar <ilemarandrade@gmail.com>
2023-03-15 20:11:05 -0400

    feat: create hook to get characters

---
+++ src/hooks/api/useGetCharacter.ts
@@ -0,0 +1,33 @@
+import { useCallback, useState } from "react";
+import {
+  ICharacters,
+  IResponseCharactersEndpoint,
+} from "../../components/models/endPointsModel";
+import handleRequest from "../../utils/handleRequest";
+
+const useGetCharacters = () => {
+  const [data, setData] = useState<ICharacters[] | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const getCharacters = useCallback(async () => {
+    setIsLoading(true);
+    handleRequest({
+      method: "get",
+      url: `/character`,
+      onSuccess: ({ data }: { data: IResponseCharactersEndpoint }) => {
+        if (data?.results) {
+          setData(data.results);
+          setIsLoading(false);
+          return;
+        }
+        setIsLoading(false);
+      },
+      onError: () => {
+        setIsLoading(false);
+      },
+    });
+  }, []);
+
+  return { getCharacters, data, isLoading };
+};
+
+export default useGetCharacters;
