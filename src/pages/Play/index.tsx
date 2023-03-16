9a49eec0ee182ad04c54260e41480aa19765800e
Ilemar <ilemarandrade@gmail.com>
2023-03-15 23:04:12 -0400

    feat: add characters random in game board

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from "react";
 import GameBoard from "../../components/GameBoard";
-import data from "../../__test__/data/characters.json";
+import { ICharacters } from "../../components/models/endPointsModel";
+import { useCharactersState } from "../../contexts/CharactersContext";
+
+const randomCharacters = (characters: ICharacters[] | null) => {
+  if (characters) {
+    return [...characters?.slice(0, 6), ...characters?.slice(0, 6)]?.sort(
+      () => 0.5 - Math.random()
+    );
+  }
+  return;
+};
 
 const Play = () => {
+  const { characters } = useCharactersState();
+  const [charactersState, setCharactersState] = useState(() => {
+    const value = randomCharacters(characters)?.map(({ ...keys }, index) => ({
+      ...keys,
+      open: true,
+      position: index,
+    }));
+    return value;
+  });
+
   return (
     <>
       <div className="d-flex jc-space-between">
         <h2 className="mb-1-5">Aciertos:</h2>
         <h2 className="mb-1-5">Turnos:</h2>
       </div>
-      <GameBoard data={data.results} />
+      {charactersState && <GameBoard data={charactersState} />}
     </>
   );
 };
