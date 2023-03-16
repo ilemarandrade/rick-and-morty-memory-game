b6cfa8040d878060f8dbdf60fb71031db8eabb27
Ilemar <ilemarandrade@gmail.com>
2023-03-15 23:07:23 -0400

    feat: open card each click

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import GameBoard from "../../components/GameBoard";
 import { ICharacters } from "../../components/models/endPointsModel";
+import { AllNumberKeys } from "../../components/models/generics";
 import { useCharactersState } from "../../contexts/CharactersContext";
 
 const randomCharacters = (characters: ICharacters[] | null) => {
@@ -31,6 +32,16 @@ const Play = () => {
     );
   };
 
+  const openCard = ({ position, id }: AllNumberKeys) => {
+    setCharactersState((prev) =>
+      prev?.map((currentCharacter) =>
+        currentCharacter.position === position
+          ? { ...currentCharacter, open: true }
+          : currentCharacter
+      )
+    );
+  };
+
   useEffect(() => {
     setTimeout(() => {
       closeAll();
@@ -44,7 +55,9 @@ const Play = () => {
         <h2 className="mb-1-5">Aciertos:</h2>
         <h2 className="mb-1-5">Turnos:</h2>
       </div>
-      {charactersState && <GameBoard data={charactersState} />}
+      {charactersState && (
+        <GameBoard data={charactersState} onClickToCard={openCard} />
+      )}
     </>
   );
 };
