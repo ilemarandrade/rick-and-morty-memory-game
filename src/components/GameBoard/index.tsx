fdfee4224c3b1b57865f0739138b1833f605ad21
Ilemar <ilemarandrade@gmail.com>
2023-03-17 14:18:10 -0400

    refactor GameBoard component

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -1,27 +1,30 @@
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import Card from "../Card";
 import { AllNumberKeys } from "../../models/generics";
 import classes from "./GameBoard.module.scss";
-interface Props {
-  data: {
-    name: string;
-    image: string;
-    status: string;
-    species: string;
-    id: number;
-    onClick?: (data: AllNumberKeys) => void;
-    open?: boolean;
-    position?: number;
-    wasFound?: boolean;
-  }[];
-  onClickToCard?: any;
+import { ICharacters } from "../../models/endPointsModel";
+
+interface ICard extends ICharacters {
+  open?: boolean;
+  position?: number;
+  wasFound?: boolean;
 }
 
-const GameBoard = ({ data, onClickToCard }: Props) => {
+interface IGameBoard {
+  cardsData: ICard[];
+  onClickToCard?: ({ id, position }: AllNumberKeys) => void;
+}
+
+const GameBoard = ({ cardsData, onClickToCard = () => {} }: IGameBoard) => {
+  const handlerOnClick = useCallback(
+    ({ id, position }: AllNumberKeys) => onClickToCard({ id, position }),
+    [onClickToCard]
+  );
+
   const cards = useMemo(
     () => (
       <>
-        {data.map(
+        {cardsData.map(
           ({
             name,
             image,
@@ -33,20 +36,21 @@ const GameBoard = ({ data, onClickToCard }: Props) => {
             wasFound = false,
           }) => (
             <Card
-              key={id + position + name}
+              key={`${id}-${position}-${name}`}
               name={name}
               img={image}
               open={open}
               origin={`${status} - ${species}`}
-              onClick={() => onClickToCard && onClickToCard({ id, position })}
+              onClick={() => handlerOnClick({ id, position })}
               wasFound={wasFound}
             />
           )
         )}
       </>
     ),
-    [data, onClickToCard]
+    [cardsData, handlerOnClick]
   );
+
   return <div className={classes.root}>{cards}</div>;
 };
 export default GameBoard;
