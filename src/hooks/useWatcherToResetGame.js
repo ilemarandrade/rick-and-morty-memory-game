9fe5b2dd2d88eb3ec6eb0d64337fa4e2d9ab860c
Ilemar <ilemarandrade@gmail.com>
2023-03-17 22:07:19 -0400

    feat: hook to handle when reset game

---
+++ src/hooks/useWatcherToResetGame.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router";
+import routes from "../constants/routes";
+import { useGameControlState } from "../contexts/GameControlContext";
+
+const useWatcherToResetGame = () => {
+  const { setMatchesGot, setTurns } = useGameControlState();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname !== routes.PLAY) {
+      setMatchesGot(0);
+    } else {
+      setTurns(0);
+    }
+  }, [pathname, setMatchesGot, setTurns]);
+};
+
+export default useWatcherToResetGame;
