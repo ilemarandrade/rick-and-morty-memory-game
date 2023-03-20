34910d8f7699d23677b9fc7ae25a21570e945a28
Ilemar <ilemarandrade@gmail.com>
2023-03-20 00:15:40 -0400

    feat: add constants of instructions and variables to game

---
+++ src/constants/instructions.ts
@@ -0,0 +1,10 @@
+const instructions = [
+  "Tendrás 3 segundos para memorizar las cartas.",
+  "Una vez volteadas puedes seleccionar 2 cartas.",
+  "Si las cartas abiertas son iguales te sumará puntos a favor.",
+  "Si las cartas son diferentes se colocarán como estaban y podrás volver a elegir otras dos más.",
+  "Procura hacerlo la menor cantidad de intentos.",
+  "Mientras menos intentos/turnos, más memoria tienes!",
+];
+
+export default instructions;
